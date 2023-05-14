34b7519a983a388dbd9d9e11da00b8c68ebfdd93
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:27:25 +0800

    Membuat String dari array char

--- src/main/java/operator/Pbo1Pertemuan03.java
+++ src/main/java/operator/Pbo1Pertemuan03.java
@@ -2,8 +2,14 @@ package operator;
 
 public class Pbo1Pertemuan03 {
     public static void main(String[] args) {
+        String nama = "Jihad Lutfian Ardana";
         String kota = "Banjarbaru";
         
+        System.out.println(nama);
         System.out.println(kota);
+        
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
     }
 }
