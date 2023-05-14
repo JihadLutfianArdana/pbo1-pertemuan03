3d2bfa83d60af63cf3ae8159889ef6b9c53a50c7
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:50:28 +0800

    Operator Unary

---
+++ src/main/java/operator/OperatorUnary.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i+=3;
+        System.out.println(i);
+        i+=3;
+        System.out.println(i);
+        i*=3;
+        System.out.println(i);
+        i*=3;
+        System.out.println(i);
+        
+        
+    }
+}
