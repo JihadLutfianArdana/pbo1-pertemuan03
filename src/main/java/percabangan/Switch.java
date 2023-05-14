a0bdbd57c031339ac84f9c2d558667b08263be29
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 21:30:07 +0800

    Switch

---
+++ src/main/java/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int Bejalanan=1;
+        switch(Bejalanan){
+            case 1:
+                System.out.println("Urang Lapas");
+                break;
+            case 2:
+                System.out.println("Urang Lapas Jua");
+                break;
+            case 3:
+                System.out.println("Ubat Nyamuk");
+                break;
+            case 4:
+                System.out.println("Dobel Det");
+                break;
+            default:
+                System.out.println("Urang Halus");
+                break;
+        }
+    }
+}
