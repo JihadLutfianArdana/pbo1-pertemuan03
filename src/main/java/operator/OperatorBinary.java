59444b1239dbef30965f05d0872f83e4c9ea472c
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:40:30 +0800

    Operator Binary Assignment

---
+++ src/main/java/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+        String nama = "Jihad Lutfian Ardana";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.85;
+        
+        System.out.println(nama);
+        System.out.println("Jumlah SKS = " + jumlahSks);
+        System.out.println("IPK = " + indeksPrestasiKumulatif);
+    }
+}
