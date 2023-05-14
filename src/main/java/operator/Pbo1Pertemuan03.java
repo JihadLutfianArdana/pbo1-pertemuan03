6bd3c9a1fab0b099ff7c04b7e5bbf6d0a270dfd2
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:30:57 +0800

    Upper Lower String
    Upper = Huruf Besar
    Lower = Huruf Kecil
    SubString5 = 5 huruf terakhir
    SubString0,6 = 6 huruf awal

--- src/main/java/operator/Pbo1Pertemuan03.java
+++ src/main/java/operator/Pbo1Pertemuan03.java
@@ -11,5 +11,10 @@ public class Pbo1Pertemuan03 {
         char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+        
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,6));
     }
 }
