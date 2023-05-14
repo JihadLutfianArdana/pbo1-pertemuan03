70832e519515889e3274ccd6a48a75b1484e87ce
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:23:18 +0800

    Initial commit

---
+++ src/main/java/operator/Pbo1Pertemuan03.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class Pbo1Pertemuan03 {
+    public static void main(String[] args) {
+        String kota = "Banjarbaru";
+        
+        System.out.println(kota);
+    }
+}
