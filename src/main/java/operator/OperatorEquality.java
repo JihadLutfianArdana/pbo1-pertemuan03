a8832c85f7c87bf65f7ad71c760a33c24b0573ad
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 20:55:51 +0800

    Operator Equality

---
+++ src/main/java/operator/OperatorEquality.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorEquality {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 11;
+        System.out.println(i==10);
+        System.out.println(i==11);
+        System.out.println(i==j);
+        System.out.println(i>j);
+        System.out.println(i<j);
+        System.out.println(10>11);
+        System.out.println(10<11);
+        System.out.println(i!=10);
+        System.out.println(j!=10);
+        
+    }
+}
